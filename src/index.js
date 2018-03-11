118ae955c21316a8b4c590a5a63604be23cf60a0
kir-22 <kirill89kireew@gmail.com>
2018-03-11 22:23:35 +0300

    Workin is not true

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+    var number = 0;
+
+   for (var i=0;i<matrix.length;i++){
+       var len=matrix[i].length;
+       for(var j=0;j<len; j++){
+           if (matrix[i][j] == 0) matrix[i][j] = number--;
+       }
+   }
+
+    return matrix;
+
 }
